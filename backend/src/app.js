c49394edcaa1e45b5972c91a787634f3a5039c0b
Seniru <senirupasan@gmail.com>
2025-02-20 15:02:08 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,50 @@
+require("dotenv").config()
+
+const os = require("os")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+
+
+const logger = require("./utils/logger")
+const { StatusCodes } = require("http-status-codes")
+const createResponse = require("./utils/createResponse")
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+app.use(
+    morgan("dev", {
+        stream: { write: (message) => logger.info(message.trim()) },
+    }),
+)
+
+// routes
+
+app.use((err, req, res, next) => {
+    logger.error(err.stack)
+    createResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    next()
+})
+
+const start = async () => {
+    const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/test"
+    const SERVER_PORT = process.env.SERVER_PORT || 8888
+
+    logger.info("Connecting to MongoDB...")
+    await mongoose.connect(MONGO_URI)
+    logger.info("Connected to MongoDB!")
+
+    logger.info("Starting server...")
+    app.listen(SERVER_PORT, () => {
+        let networkInterfaces = os.networkInterfaces()
+        let address = networkInterfaces.wlo1 && networkInterfaces.wlo1[0].address
+        logger.info("Server listening on")
+        logger.info(`\tLocal:\thttp://127.0.0.1:${SERVER_PORT}`)
+        address && logger.info(`\tIP:\thttp://${address}:${SERVER_PORT}`)
+    })
+}
+
+start()
---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+// credits: https://www.w3schools.com/react/react_customhooks.asp
+
+import { useState, useEffect } from "react"
+
+const useFetch = (url, initialValue, refreshFlag) => {
+    const [data, setData] = useState(initialValue)
+    const [error, setError] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true)
+                let response = await fetch(url, { headers })
+                let result = await response.json()
+                if (!response.ok)
+                    throw new Error(result.body || response.statusText)
+                setData(result)
+                setIsLoading(false)
+            } catch (error) {
+                setError(error.message)
+            }
+        }
+        fetchData()
+    }, [url, token, refreshFlag])
+
+    return [data, error, isLoading]
+}
+
+export default useFetch
