c703cbf29c6e22160cf08cb6364c70e9c73d51ab
Seniru Pasan Indira <senirupasan@gmail.com>
2025-02-28 14:08:46 +0530

    feat: create user management ui (#19)
    closes #6

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,11 +1,12 @@
 import TabMenu from "../../components/TabMenu"
 import TabItem from "../../components/TabMenu/TabItem"
+import UserManagement from "./UserManagement"
 
 export default function Admin() {
   return (
     <TabMenu>
       <TabItem name="Inventory" element={<></>} />
-      <TabItem name="User management" element={<></>} />
+      <TabItem name="User management" element={<UserManagement />} />
       <TabItem name="Supply management" element={<></>} />
       <TabItem name="Sales management" element={<></>} />
     </TabMenu>
