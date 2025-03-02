a0d23f1a69e8d3f1cae783603a31e8f283d3eb68
Seniru <senirupasan@gmail.com>
2025-03-02 14:02:25 +0530

    update: forbidden page

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom"
 
 import { useAuth } from "../contexts/AuthProvider"
+import Forbidden from "../pages/errors/Forbidden"
 
 export default function PrivateRoute({ requiredRole }) {
   let { user } = useAuth()
   if (!user) return <Navigate to="/auth/login" />
 
   if (user.roles.includes(requiredRole || "USER")) return <Outlet />
-  return <Navigate to="/" />
+  return <Forbidden />
 }
