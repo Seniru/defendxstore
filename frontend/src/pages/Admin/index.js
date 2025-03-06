53404c589efaceaaf296f5626d6167ad067bb6f1
Siyumika <ssiyumika@gmail.com>
2025-03-06 11:22:58 +0530

    feat: create supply management

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,13 +1,14 @@
 import TabMenu from "../../components/TabMenu"
 import TabItem from "../../components/TabMenu/TabItem"
 import UserManagement from "./UserManagement"
+import SupplyManagement from "./SupplyManagement"
 
 export default function Admin() {
   return (
     <TabMenu>
       <TabItem name="Inventory" element={<></>} />
       <TabItem name="User management" element={<UserManagement />} />
-      <TabItem name="Supply management" element={<></>} />
+      <TabItem name="Supply management" element={<SupplyManagement />} />
       <TabItem name="Sales management" element={<></>} />
     </TabMenu>
   )
