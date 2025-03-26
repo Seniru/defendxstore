4869eba5d225fe48cd6c070e1a8e5ff739d6e40e
Siyumika <164844686+Siyumika@users.noreply.github.com>
2025-03-26 10:51:25 +0530

    feat: promo codes (#51)
    * feat: create supply management
    
    * feat:SalesManagement Tables,buttons design
    
    * update: prettify
    
    * feat:create revenue,profit,cost charts
    
    * create promocodes
    
    ---------
    
    Co-authored-by: Seniru Pasan Indira <senirupasan@gmail.com>

--- frontend/src/pages/Admin/SalesManagement.js
+++ frontend/src/pages/Admin/SalesManagement.js
@@ -1,5 +1,4 @@
 import { LineChart } from "@mui/x-charts/LineChart"
-
 import "./SalesManagement.css"
 import Select from "../../components/Select"
 import Table from "../../components/Table"
@@ -10,40 +9,26 @@ export default function SalesManagement() {
       <div className="sales-management-actions">
         View <Select items={["Expected Sales", "Revenue", "Costs", "Profit"]} />
         <br />
-        <LineChart
-          sx={{ width: "50% " }}
-          height={300}
-          series={[
-            {
-              data: [12, 32, 32, 43, 64, 43, 24, 53, 23, 54, 65, 40],
-              label: "Revenue",
-            },
-            {
-              data: [8, 24, 26, 35, 50, 36, 18, 42, 17, 45, 55, 30],
-              label: "Cost",
-            },
-            { data: [4, 8, 6, 8, 14, 7, 6, 11, 6, 9, 10, 10], label: "profit" },
-          ]}
-          xAxis={[
-            {
-              scaleType: "point",
-              data: [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "Novemeber",
-                "Decemeber",
-              ],
-            },
-          ]}
-        />
+        <div>
+
+          <div>
+            <LineChart
+              sx={{ width: "50% "}}
+              height={300}
+              series={[
+                { data: [12, 32, 32, 43, 64, 43, 24, 53, 23 ,54, 65, 40], label: "Revenue" },
+                { data: [8,24,26,35,50,36,18,42,17,45,55,30],label: "Cost" },
+                { data: [4,8,6,8,14,7,6,11,6,9,10,10],label: "profit" },
+                
+              ]}
+            
+              xAxis={[{ scaleType: "point", data: ["January","February","March","April","May","June","July","August","September","October","Novemeber","Decemeber"] }]}
+            />
+          </div>
+          <div>
+            hello world
+          </div>
+        </div>
         <h1>Monthly Sales Breakdown</h1>
       </div>
 
