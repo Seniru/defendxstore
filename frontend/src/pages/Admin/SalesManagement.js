3507914e4175203687d5f761e91caf10f19a0d0e
Seniru Pasan Indira <senirupasan@gmail.com>
2025-03-26 22:28:52 +0530

    feat: promocode cruds (#53)
    Co-authored-by: Siyumika <ssiyumika@gmail.com>

--- frontend/src/pages/Admin/SalesManagement.js
+++ frontend/src/pages/Admin/SalesManagement.js
@@ -2,6 +2,8 @@ import { LineChart } from "@mui/x-charts/LineChart"
 import "./SalesManagement.css"
 import Select from "../../components/Select"
 import Table from "../../components/Table"
+import { PieChart } from '@mui/x-charts/PieChart';
+import * as React from 'react';
 
 export default function SalesManagement() {
   return (
@@ -9,10 +11,9 @@ export default function SalesManagement() {
       <div className="sales-management-actions">
         View <Select items={["Expected Sales", "Revenue", "Costs", "Profit"]} />
         <br />
-        <div>
+        <div style={{ display: "flex" }}>
           <div>
             <LineChart
-              sx={{ width: "50% " }}
               height={300}
               series={[
                 {
@@ -49,7 +50,21 @@ export default function SalesManagement() {
               ]}
             />
           </div>
-          <div>hello world</div>
+          <div>
+          <PieChart
+  series={[
+    {
+      data: [
+        { id: 0, value: 10, label: 'series A' },
+        { id: 1, value: 15, label: 'series B' },
+        { id: 2, value: 20, label: 'series C' },
+      ],
+    },
+  ]}
+  width={400}
+  height={200}
+/>
+        </div>
         </div>
         <h1>Monthly Sales Breakdown</h1>
       </div>
