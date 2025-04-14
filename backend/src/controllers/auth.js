491277ca69e2821d1a088172f4786284b073d950
Seniru <senirupasan@gmail.com>
2025-04-14 13:52:07 +0530

    update: display error messages when logging in

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -10,13 +10,20 @@ const createToken = require("../utils/createToken")
 const login = async (req, res) => {
     let { email, password } = req.body
     let user = await User.findOne({ email }).exec()
-    if (!user) return createResponse(res, StatusCodes.NOT_FOUND, "User not found")
+    if (!user)
+        return createResponse(res, StatusCodes.NOT_FOUND, {
+            field: "email",
+            message: "User not found",
+        })
 
     if (bcrypt.compareSync(password, user.password)) {
         let token = createToken(user)
         return createResponse(res, StatusCodes.OK, { token })
     }
-    return createResponse(res, StatusCodes.UNAUTHORIZED, "Invalid password")
+    return createResponse(res, StatusCodes.UNAUTHORIZED, {
+        field: "password",
+        message: "Invalid password",
+    })
 }
 
 module.exports = {
