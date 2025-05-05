12be0b6f4734202c40714b766021fa5edc9e30c3
Seniru <senirupasan@gmail.com>
2025-05-05 18:50:24 +0530

    feat: reset forgotten password

--- frontend/src/forms/ForgotPasswordForm/index.js
+++ frontend/src/forms/ForgotPasswordForm/index.js
@@ -1,21 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Input from "../../components/Input"
-import { faInfoCircle, faPhone } from "@fortawesome/free-solid-svg-icons"
+import {
+  faCheckCircle,
+  faEnvelope,
+  faInfoCircle,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons"
 import Button from "../../components/Button"
+import { useRef, useState } from "react"
+import api from "../../utils/api"
 
 import "./ForgotPasswordForm.css"
-import { useRef, useState } from "react"
 
 export default function ForgotPasswordForm() {
   const emailRef = useRef()
   const [emailError, setEmailError] = useState(null)
+  const [actionDone, setActionDone] = useState(false)
 
   const handleSubmit = async (evt) => {
     evt.preventDefault()
+    if (actionDone) return
 
     const isEmailValid = emailRef.current.validity.valid
     setEmailError(isEmailValid ? null : emailRef.current.validationMessage)
     if (!isEmailValid) return
+
+    await api.post("/api/auth/forgot-password", {
+      email: emailRef.current.value,
+    })
+    setActionDone(true)
   }
 
   return (
@@ -49,8 +62,18 @@ export default function ForgotPasswordForm() {
         ref={emailRef}
         required
       />
-      <Button type="submit" kind="primary">
-        Send password reset email
+
+      <Button type="submit" kind="primary" disabled={actionDone}>
+        {actionDone ? (
+          <>
+            <FontAwesomeIcon icon={faCheckCircle} /> A reset link has been sent
+            to the verified email address
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faEnvelope} /> Send password reset email
+          </>
+        )}
       </Button>
     </form>
   )
